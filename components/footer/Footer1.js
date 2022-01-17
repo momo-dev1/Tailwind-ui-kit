9a9e3bc5d62be319d7d7ff51dd6817dbb7078310
momo-dev1 <didy1234567@gmail.com>
2022-01-17 16:59:21 +0200

    footer1

--- components/footer/Footer1.js
+++ components/footer/Footer1.js
@@ -1,32 +1,32 @@
 const Footer1 = ({}) => {
   return (
     <div className="dark:bg-gray-900">
-      <section className="pb-14 pt-24 flex justify-center items-center bg-gray-100 dark:bg-gray-900">
-        <footer className="mt-20 mx-auto container md:w-full flex justify-center items-center">
-          <div className="flex xl:flex-row flex-col lg:gap-x-24 px-6 md:px-0">
-            <div className=" flex-col flex justify-start items-start lg:w-80">
-              <div className="xl:flex-col flex justify-start items-center xl:items-start">
+      <section className="flex items-center justify-center pt-24 bg-gray-100 pb-14 dark:bg-gray-900">
+        <footer className="container flex items-center justify-center mx-auto mt-20 md:w-full">
+          <div className="flex flex-col px-6 xl:flex-row lg:gap-x-24 md:px-0">
+            <div className="flex flex-col items-start justify-start lg:w-80">
+              <div className="flex items-center justify-start xl:flex-col xl:items-start">
                 <div className="flex justify-center mr-6 xl:mr-0 dark:text-white text-gray-800 w-[74px] h-[65x]">
                   Logo
                 </div>
-                <div className="flex justify-center flex-col">
+                <div className="flex flex-col justify-center">
                   <div className="xl:mt-8">
-                    <p className="text-gray-600 dark:text-gray-300 text-sm leading-normal">
+                    <p className="text-sm leading-normal text-gray-600 dark:text-gray-300">
                       Copyright © 2021 companyName.
                     </p>
                   </div>
                   <div className="mt-2">
-                    <p className="text-gray-600 dark:text-gray-300 text-sm leading-normal">
+                    <p className="text-sm leading-normal text-gray-600 dark:text-gray-300">
                       All rights reserved
                     </p>
                   </div>
                 </div>
               </div>
-              <div className="mt-8 flex justify-start items-start flex-row lg:space-x-4 space-x-2">
+              <div className="flex flex-row items-start justify-start mt-8 space-x-2 lg:space-x-4">
                 <button
                   aria-label="twitter"
                   role="button"
-                  className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-800 hover:text-blue-600  text-gray-800 dark:text-white"
+                  className="text-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-800 hover:text-blue-600 dark:text-white"
                 >
                   <svg
                     className="fill-current"
@@ -40,7 +40,7 @@ const Footer1 = ({}) => {
                 <button
                   aria-label="youtube"
                   role="button"
-                  className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-800 hover:text-blue-400 text-gray-800 dark:text-white"
+                  className="text-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-800 hover:text-blue-400 dark:text-white"
                 >
                   <svg
                     className="fill-current"
@@ -53,17 +53,17 @@ const Footer1 = ({}) => {
                 </button>
               </div>
             </div>
-            <div className="flex flex-row mt-6  xl:mt-0 lg:gap-x-14 gap-x-10">
-              <div className="grid lg:grid-cols-3 grid-cols-2 gap-y-10 lg:gap-y-0 gap-x-0 lg:gap-x-0">
+            <div className="flex flex-row mt-6 xl:mt-0 lg:gap-x-14 gap-x-10">
+              <div className="grid grid-cols-2 lg:grid-cols-3 gap-y-10 lg:gap-y-0 gap-x-0 lg:gap-x-0">
                 <div className="lg:w-40">
-                  <h2 className="text-gray-800 dark:text-white text-lg md:text-xl font-medium leading-loose">
+                  <h2 className="text-lg font-medium leading-loose text-gray-800 dark:text-white md:text-xl">
                     Company
                   </h2>
-                  <div className="mt-6 flex justify-start items-start flex-col space-y-2">
+                  <div className="flex flex-col items-start justify-start mt-6 space-y-2">
                     <div>
                       <a
                         href="#!"
-                        className="text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 text-sm md:text-base leading-relaxed"
+                        className="text-sm leading-relaxed text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 md:text-base"
                       >
                         About us
                       </a>
@@ -71,7 +71,7 @@ const Footer1 = ({}) => {
                     <div>
                       <a
                         href="#!"
-                        className="text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 text-sm md:text-base leading-relaxed"
+                        className="text-sm leading-relaxed text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 md:text-base"
                       >
                         Blog
                       </a>
@@ -79,7 +79,7 @@ const Footer1 = ({}) => {
                     <div>
                       <a
                         href="#!"
-                        className="text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 text-sm md:text-base leading-relaxed"
+                        className="text-sm leading-relaxed text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 md:text-base"
                       >
                         Contact us
                       </a>
@@ -87,7 +87,7 @@ const Footer1 = ({}) => {
                     <div>
                       <a
                         href="#!"
-                        className="text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 text-sm md:text-base leading-relaxed"
+                        className="text-sm leading-relaxed text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 md:text-base"
                       >
                         Pricing
                       </a>
@@ -95,7 +95,7 @@ const Footer1 = ({}) => {
                     <div>
                       <a
                         href="#!"
-                        className="text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 text-sm md:text-base leading-relaxed"
+                        className="text-sm leading-relaxed text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 md:text-base"
                       >
                         Testimonials
                       </a>
@@ -103,14 +103,14 @@ const Footer1 = ({}) => {
                   </div>
                 </div>
                 <div className="lg:w-40">
-                  <h2 className="text-gray-800 dark:text-white text-lg md:text-xl font-medium leading-loose">
+                  <h2 className="text-lg font-medium leading-loose text-gray-800 dark:text-white md:text-xl">
                     Support
                   </h2>
-                  <div className="mt-6 flex  justify-start items-start flex-col space-y-2">
+                  <div className="flex flex-col items-start justify-start mt-6 space-y-2">
                     <div>
                       <a
                         href="#!"
-                        className="text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 text-sm md:text-base leading-relaxed"
+                        className="text-sm leading-relaxed text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 md:text-base"
                       >
                         Help center
                       </a>
@@ -118,7 +118,7 @@ const Footer1 = ({}) => {
                     <div>
                       <a
                         href="#!"
-                        className="text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 text-sm md:text-base leading-relaxed"
+                        className="text-sm leading-relaxed text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 md:text-base"
                       >
                         Terms of service
                       </a>
@@ -126,7 +126,7 @@ const Footer1 = ({}) => {
                     <div>
                       <a
                         href="#!"
-                        className="text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 text-sm md:text-base leading-relaxed"
+                        className="text-sm leading-relaxed text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 md:text-base"
                       >
                         Legal
                       </a>
@@ -134,7 +134,7 @@ const Footer1 = ({}) => {
                     <div>
                       <a
                         href="#!"
-                        className="text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 text-sm md:text-base leading-relaxed"
+                        className="text-sm leading-relaxed text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 md:text-base"
                       >
                         Privacy policy
                       </a>
@@ -142,29 +142,29 @@ const Footer1 = ({}) => {
                     <div>
                       <a
                         href="#!"
-                        className="text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 text-sm md:text-base leading-relaxed"
+                        className="text-sm leading-relaxed text-gray-800 dark:text-white focus:outline-none focus:opacity-75 hover:opacity-75 md:text-base"
                       >
                         Status
                       </a>
                     </div>
                   </div>
                 </div>
-                <div className="sm:block hidden">
-                  <h2 className="w-60 text-lg md:text-xl font-medium leading-loose text-gray-800 dark:text-white">
+                <div className="hidden sm:block">
+                  <h2 className="text-lg font-medium leading-loose text-gray-800 w-60 md:text-xl dark:text-white">
                     Get Updates and more
                   </h2>
-                  <div className="dark:bg-gray-800 bg-white rounded-lg mt-6 flex flex justify-start items-start flex-col space-y-2">
-                    <div className="w-full flex justify-between items-center space-x-2 sm:space-x-0">
+                  <div className="flex flex-col items-start justify-start mt-6 space-y-2 bg-white rounded-lg dark:bg-gray-800">
+                    <div className="flex items-center justify-between w-full space-x-2 sm:space-x-0">
                       <div className="relative w-full">
                         <input
-                          className="bg-transparent focus:outline-none focus:ring-2 w-full focus:ring-gray-400 h-10 p-2 dark:placeholder-gray-300 placeholder-gray-600 text-xs md:text-base"
+                          className="w-full h-10 p-2 text-xs placeholder-gray-600 bg-transparent focus:outline-none focus:ring-2 focus:ring-gray-400 dark:placeholder-gray-300 md:text-base"
                           type="text"
                           placeholder="Your email address"
                         />
                         <button
                           aria-label="send email"
                           role="button"
-                          className="absolute right-0 top-0 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 hover:opacity-75 p-2 sm:p-0 sm:w-12 h-10 flex justify-center items-center bg-gradient-to-l from-indigo-600 to-indigo-700 rounded-sm"
+                          className="absolute top-0 right-0 flex items-center justify-center h-10 p-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 hover:opacity-75 sm:p-0 sm:w-12 bg-gradient-to-l from-indigo-600 to-indigo-700"
                         >
                           <svg
                             width={26}
@@ -198,19 +198,19 @@ const Footer1 = ({}) => {
                   </div>
                 </div>
               </div>
-              <div className="sm:hidden sm:mt-0 mt-4">
-                <h2 className="w-60 text-lg md:text-xl font-medium leading-loose text-gray-800 dark:text-white">
+              <div className="mt-4 sm:hidden sm:mt-0">
+                <h2 className="text-lg font-medium leading-loose text-gray-800 w-60 md:text-xl dark:text-white">
                   Get Updates and more
                 </h2>
-                <div className="dark:bg-gray-800 bg-white rounded-lg mt-6 flex flex justify-start items-start flex-col space-y-2">
-                  <div className="w-full flex justify-between items-center space-x-2 sm:space-x-0">
+                <div className="flex flex-col items-start justify-start mt-6 space-y-2 bg-white rounded-lg dark:bg-gray-800">
+                  <div className="flex items-center justify-between w-full space-x-2 sm:space-x-0">
                     <div className="relative w-full">
                       <input
-                        className="bg-transparent focus:outline-none focus:ring-2 w-full focus:ring-gray-400 h-10 p-2 dark:placeholder-gray-300 placeholder-gray-600 text-xs md:text-base"
+                        className="w-full h-10 p-2 text-xs placeholder-gray-600 bg-transparent focus:outline-none focus:ring-2 focus:ring-gray-400 dark:placeholder-gray-300 md:text-base"
                         type="text"
                         placeholder="Your email address"
                       />
-                      <button className="absolute right-0 top-0 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 hover:opacity-75 p-2 sm:p-0 sm:w-12 h-10 flex justify-center items-center bg-gradient-to-l from-indigo-600 to-indigo-700 rounded-sm">
+                      <button className="absolute top-0 right-0 flex items-center justify-center h-10 p-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 hover:opacity-75 sm:p-0 sm:w-12 bg-gradient-to-l from-indigo-600 to-indigo-700">
                         <svg
                           width={26}
                           height={26}
