e4067c551386b52577417c11da5a9e5f57d11356
momo-dev1 <didy1234567@gmail.com>
2022-01-26 21:12:42 +0200

    about page

--- pages/components/about/index.js
+++ pages/components/about/index.js
@@ -1,5 +1,6 @@
 import ComponentLayout from "../../../components/layout/ComponentLayout";
 import About1 from "../../../components/kit/ui/about/About1";
+import About2 from "../../../components/kit/ui/about/About2";
 
 const AboutPage = ({}) => {
   return (
@@ -9,6 +10,7 @@ const AboutPage = ({}) => {
     >
       <SectionHeader title="About" />
       <ComponentLayout title="about1 1" vertical={false} element={<About1 />} />
+      <ComponentLayout title="about1 2" vertical={false} element={<About2 />} />
     </AppLayout>
   );
 };
